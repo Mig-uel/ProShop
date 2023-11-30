d85bb577008ce1ef13ef04bc9f584bdd07aa5345
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-30 16:43:37 -0500

    created a component for a single product.

---
+++ frontend/src/components/product.component.jsx
@@ -0,0 +1,23 @@
+import { Card } from 'react-bootstrap'
+
+const Product = ({ product: { _id, image, name, price } }) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${_id}`}>
+        <Card.Img src={image} variant='top' />
+      </a>
+
+      <Card.Body>
+        <a href={`/producs/${_id}`}>
+          <Card.Title as='div'>
+            <strong>{name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as='h3'>${price}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product

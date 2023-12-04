0aaec6c513c1a5bc01dec29fa5613361b5441f78
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-04 14:32:12 -0500

    Add morgan middleware for HTTP request logging

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 const products = require('./data/products')
 
@@ -7,6 +8,9 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
 const env = process.env.NODE_ENV
+
+// middleware
+app.use(morgan('dev')) // <--- HTTP request logger using morgan
 
 // all products
 app.get('/api/products', (req, res) => {

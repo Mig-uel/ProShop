7167dfe3a731e2651d44c430b5e7c73a2aebf5cc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-04 14:37:28 -0500

    Add proxy configuration for API and uploads

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // proxy requests prefixed '/api' and '/uploads'
+    // setting up proxy so we don't have to keep typing out full api address 'http://localhost
+    
+    '/api': 'http://localhost:5000',
+    '/uploads': 'http://localhost:5000',
+  },
 })

e97c977e90bcc16b907edfee298f114ac91420df
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-05 16:29:06 -0500

    Add errorHandler middleware

---
+++ backend/middleware/errorHandler.middleware.js
@@ -0,0 +1,26 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found: ${req.originalUrl}`)
+
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  let message = err.message
+
+  // check for mongoose bad ObjectId (CastError)
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    message = `Resource not found`
+    statusCode = 404
+  }
+
+  res
+    .status(statusCode)
+    .json({ stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack })
+}
+
+module.exports = {
+  notFound,
+  errorHandler,
+}

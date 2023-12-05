340eced9bf0c337c2763425011ade84ff7419071
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-05 16:28:57 -0500

    Refactor error handling in asyncHandler middleware

--- backend/middleware/asyncHandler.middleware.js
+++ backend/middleware/asyncHandler.middleware.js
@@ -1,7 +1,9 @@
 const asyncHandler = (func) => {
   return (req, res, next) => {
-    Promise.resolve(func(req, res, next)).catch(next)
+    func(req, res, next).catch(next)
   }
 }
+
+// catches the error and passes it to the 'next()' function which then passes it to the error handler
 
 module.exports = asyncHandler

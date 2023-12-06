b3b090bb44d52c495dec8b300e1a27d8528acf0c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-06 14:03:53 -0500

    Refactor home screen component to use rtk-query

--- frontend/src/screens/home.screen.jsx
+++ frontend/src/screens/home.screen.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { useGetProductsQuery } from '../redux/slices/productsApi.slice'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/product.component'
 
 const Home = () => {
-  const [products, setProducts] = useState(useLoaderData())
+  const { data: products, isLoading, isError, error } = useGetProductsQuery()
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : isError ? (
+        <p>{error?.data?.message || error.error}</p>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   )
 }

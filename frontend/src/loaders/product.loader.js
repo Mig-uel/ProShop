04e57150e54d286b167ed5b51861a56fda2ec671
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-06 14:04:30 -0500

    Delete product and products loaders

--- frontend/src/loaders/product.loader.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const fetchProduct = async ({ params: { id } }) => {
-  const { data } = await axios.get('/api/products/' + id)
-  return data
-}
-
-export default fetchProduct
--- frontend/src/loaders/products.loader.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const fetchProducts = async () => {
-  const { data } = await axios.get('/api/products')
-
-  return data
-}
-
-export default fetchProducts

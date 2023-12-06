2f10d5830c86e66fad2e2b5287164eebda9104b9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-06 12:59:26 -0500

    Refactor Vite config to include environment variables and proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // proxy requests prefixed '/api' and '/uploads'
-    // setting up proxy so we don't have to keep typing out full api address 'http://localhost
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      // 'process.env': {},
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+    },
+
+    plugins: [react()],
+    server: {
+      // proxy requests prefixed '/api' and '/uploads'
+      // setting up proxy so we don't have to keep typing out full api address 'http://localhost
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/uploads': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
 })

353b1de81f3c8070acd1b9b7f36da62bf6856716
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-06 16:45:13 -0500

    Refactor addToCart function to use updateCart utility

--- frontend/src/redux/slices/cart.slice.js
+++ frontend/src/redux/slices/cart.slice.js
@@ -1,17 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { updateCart } from '../../utils/cart.utils'
 
 // initial state from localStorage if any or if not initialize empty array
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] }
 
-const addDecimals = (num) => (Math.round(num * 100) / 100).toFixed(2)
-
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart: (state, { payload, type }) => {
+    addToCart: (state, { payload }) => {
       const doesItemExist = state.cartItems.find((c) => c._id === payload._id)
 
       if (doesItemExist)
@@ -20,25 +19,7 @@ const cartSlice = createSlice({
         )
       else state.cartItems = [...state.cartItems, payload]
 
-      // calc items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      )
-
-      // calc shipping price (if order is over $100 = free ship, else ship = $10)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-
-      // calc tax price (15%)
-      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2))
-
-      // calc total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2)
-
-      localStorage.setItem('cart', JSON.stringify(state))
+      return updateCart(state)
     },
   },
 })

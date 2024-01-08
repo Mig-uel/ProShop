df8a24265aaa7c93f8e8534fd080512630590f02
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-01-07 19:17:14 -0500

    Add ShippingScreen component to router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import ProductScreen from './screens/product.screen.jsx'
 import CartScreen from './screens/cart.screen.jsx'
 import LoginScreen from './screens/login.screen.jsx'
 import RegisterScreen from './screens/register.screen.jsx'
+import ShippingScreen from './screens/shipping.screen.jsx'
 
 // redux/state
 import { Provider } from 'react-redux'
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
       <Route path='cart' element={<CartScreen />} />
       <Route path='login' element={<LoginScreen />} />
       <Route path='register' element={<RegisterScreen />} />
+      <Route path='shipping' element={<ShippingScreen />} />
     </Route>
   )
 )
